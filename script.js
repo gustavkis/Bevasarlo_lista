411958ee3877510dde75c49e289ae7be5e3f010b
gustavkis <gustavkis.bp@gmail.com>
2022-11-27 15:21:41 +0100

    js - Az értékek beolvasása html-ből

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function hozzaAd() {
+
+    // Az értékek beolvasása html-ből
+    
+    const tetelN = document.getElementById("nev").value;
+    const tetelSz = document.getElementById("darabSzam").value;
+    const tetelA = document.getElementById("tetelAra").value;
+
+
+
+
+}
